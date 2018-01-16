13ff6481b03c05889cb57e8c2667f771074c2b26
Hampus Malmberg <malmberg@isi.ee.ethz.ch>
2018-01-16 17:36:26 +0100

    Implemented adaptive Runge-Kutta

--- ode/ode.go
+++ ode/ode.go
@@ -5,6 +5,9 @@
 package ode
 
 import (
+	"errors"
+	"math"
+
 	"github.com/hammal/adc/ssm"
 	"gonum.org/v1/gonum/mat"
 )
@@ -17,7 +20,7 @@ type RungeKutta struct {
 // Compute the update for a Runge-Kutta system based on a current value at t = from
 // , a target time t = to, a initial value x(t=from) = value and a system model ode.
 // When algorithm is finished the result is copied into the value.
-func (rk RungeKutta) Compute(from, to float64, value *mat.VecDense, ode ssm.StateSpaceModel) {
+func (rk RungeKutta) Compute(from, to float64, value *mat.VecDense, ode ssm.StateSpaceModel) *mat.VecDense {
 	// State order
 	M, _ := value.Dims()
 	// The precomputed derivative points
@@ -25,7 +28,7 @@ func (rk RungeKutta) Compute(from, to float64, value *mat.VecDense, ode ssm.Stat
 	// Step length
 	h := to - from
 	for index := range K {
-		// Initalise an intermediate vector
+		// Initialize an intermediate vector
 		tempV := mat.NewVecDense(M, nil)
 		tempV.CopyVec(value)
 		// Compute the relevant vector by combining previously computed derivate points
@@ -36,23 +39,86 @@ func (rk RungeKutta) Compute(from, to float64, value *mat.VecDense, ode ssm.Stat
 		// Insert the new derivate point
 		K[index] = ode.StateDerivative(from+h*rk.Description.nodes[index], tempV)
 	}
-
+	// Initialize the error vector
+	err := mat.NewVecDense(M, nil)
 	// Sum up the different contributions with relevant weights.
 	for index, k := range K {
-		value.AddScaledVec(value, h*rk.Description.weights[index], k)
+		value.AddScaledVec(value, h*rk.Description.weights[0][index], k)
+		// If the Butcher Tableau allows for adaptive error computation
+		if len(rk.Description.weights) == 2 {
+			err.AddScaledVec(err, h*(rk.Description.weights[1][index]-rk.Description.weights[0][index]), k)
+		}
 	}
+	return err
+}
+
+// This adaptive Runge Kutta version implies an
+func (rk RungeKutta) AdaptiveCompute(from, to, err float64, value *mat.VecDense, ode ssm.StateSpaceModel) error {
+	var (
+		tmpState           *mat.VecDense
+		currentErrorVector *mat.VecDense
+		currentError       float64
+		tnow, tnext        float64
+		count              int
+	)
+	// Set max number of iterations
+	const maxNumberOfIterations int = 10000
+
+	// Initialize current time
+	tnow = from
+
+	M, _ := value.Dims()
+	tmpState = mat.NewVecDense(M, nil)
+
+	// Repeat until time to is reached
+	for tnow < to {
+		// Set target time
+		tnext = to
+		// Repeat until target error is reached
+		for true {
+			// Copy the current state into tmpState
+			tmpState.CopyVec(value)
+			// Execute the Runge Kutta computation
+			currentErrorVector = rk.Compute(tnow, tnext, tmpState, ode)
+			// Reset and compute Error
+			currentError = 0.
+			// count = 0
+			for index := 0; index < tmpState.Len(); index++ {
+				currentError += math.Abs(currentErrorVector.AtVec(index))
+			}
+			// Has the target error been achived?
+			// fmt.Printf("Err %v, time %v\n", currentError, tnow)
+			if currentError < err {
+				break
+			}
+			// Half the next integration interval and try again
+			tnext = (tnext-tnow)/2. + tnow
+
+			// Increment counter and check if we are allowed more trials
+			count += 1
+			if count >= maxNumberOfIterations {
+				return errors.New("Max number of iterations reached adaptive Runge-Kutta doesn't converge.")
+			}
+		}
+		// Save this state and update tnow
+		value.CopyVec(tmpState)
+		tnow = tnext
+
+	}
+	// Successful integration!  Return nil error
+	return nil
 }
 
 // NewRK4 function returns a forth order Runge-Kutta object
 func NewRK4() *RungeKutta {
 	var temp butcherTableau
 	temp.stages = 4
-	temp.nodes = []float64{0, 0.5, 0.5, 1}
-	temp.weights = []float64{1. / 6, 1. / 3, 1. / 3, 1. / 6}
+	temp.nodes = []float64{0, 1. / 2., 1. / 2., 1}
+	temp.weights = [][]float64{{1. / 6., 1. / 3., 1. / 3., 1. / 6.}}
 	temp.rungeKuttaMatrix = [][]float64{
 		nil,
-		{1. / 2},
-		{0, 1. / 2},
+		{1. / 2.},
+		{0, 1. / 2.},
 		{0, 0, 1.},
 	}
 	rk := RungeKutta{temp}
@@ -64,7 +130,7 @@ func NewEulerMethod() *RungeKutta {
 	var temp butcherTableau
 	temp.stages = 1
 	temp.nodes = []float64{0}
-	temp.weights = []float64{1}
+	temp.weights = [][]float64{{1}}
 	rk := RungeKutta{temp}
 	return &rk
 }
@@ -72,7 +138,28 @@ func NewEulerMethod() *RungeKutta {
 // butcherTableau which describes the approximate solution, see https://en.wikipedia.org/wiki/Runge–Kutta_methods.
 type butcherTableau struct {
 	stages           int
-	weights          []float64
+	weights          [][]float64
 	nodes            []float64
 	rungeKuttaMatrix [][]float64
+}
+
+// NewFehlberg45 implements https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta%E2%80%93Fehlberg_method
+func NewFehlberg45() *RungeKutta {
+	var temp butcherTableau
+	temp.stages = 6
+	temp.nodes = []float64{0, 1. / 4., 3. / 8., 12. / 13., 1., 1. / 2.}
+	temp.weights = [][]float64{
+		{16. / 135., 0, 6656. / 12825., 28561. / 56430., -9. / 50., 2. / 55.},
+		{25. / 216., 0, 1408. / 2565., 2197. / 4104., -1. / 5., 0},
+	}
+	temp.rungeKuttaMatrix = [][]float64{
+		nil,
+		{1. / 4.},
+		{3. / 32., 9. / 32.},
+		{1932. / 2197., -7200. / 2197., 7296. / 2197.},
+		{439. / 216., -8., 3680. / 513., -845. / 4104.},
+		{-8. / 27., 2, -3544. / 2565., 1859. / 4104., -11. / 40.},
+	}
+	rk := RungeKutta{temp}
+	return &rk
 }
--- ode/ode_test.go
+++ ode/ode_test.go
@@ -24,6 +24,13 @@ func TestEuler(t *testing.T) {
 	}
 }
 
+func TestFehlberg(t *testing.T) {
+	test := NewFehlberg45()
+	if test.Description.stages != 6 {
+		t.Error("Wrong number of stages.")
+	}
+}
+
 func TestCompute(y *testing.T) {
 	odeObject := NewRK4()
 	t0, t1 := 0., 1.234567891
@@ -34,9 +41,64 @@ func TestCompute(y *testing.T) {
 	}
 	stateTransitionMatrix := mat.NewDense(9, 9, data)
 	inputvector := mat.NewVecDense(9, []float64{1, 0, 0, 0, 0, 0, 0, 0, 0})
-	inputs := signal.NewInput(func(arg1 float64) float64 { return arg1 }, inputvector)
+	inputs := make([]signal.VectorFunction, 1)
+	inputs[0] = signal.NewInput(func(arg1 float64) float64 { return arg1 }, inputvector)
 	observationVector := mat.NewDense(1, 9, []float64{0, 0, 0, 0, 0, 0, 0, 0, 1})
 	sys := ssm.NewLinearStateSpaceModel(stateTransitionMatrix, observationVector, inputs)
 	odeObject.Compute(t0, t1, initState, sys)
+	fmt.Println(mat.Formatted(initState))
+}
+
+func TestAdaptiveCompute(y *testing.T) {
+	odeObject := NewFehlberg45()
+	t0, t1 := 0., 1.
+	err := 1e-4
+	initState := mat.NewVecDense(9, []float64{1.2, 32., 34, 12, 532, 12, 35, 1, 0.91283})
+	data := make([]float64, 9*9)
+	for index := 0; index < 9*9; index++ {
+		data[index] = rand.Float64()
+	}
+	stateTransitionMatrix := mat.NewDense(9, 9, data)
+	inputvector := mat.NewVecDense(9, []float64{1, 0, 0, 0, 0, 0, 0, 0, 0})
+	inputs := make([]signal.VectorFunction, 1)
+	inputs[0] = signal.NewInput(func(arg1 float64) float64 { return 1.24 }, inputvector)
+
+	observationVector := mat.NewDense(1, 9, []float64{0, 0, 0, 0, 0, 0, 0, 0, 1})
+
+	sys := ssm.NewLinearStateSpaceModel(stateTransitionMatrix, observationVector, inputs)
+
+	sucess := odeObject.AdaptiveCompute(t0, t1, err, initState, sys)
+
+	if sucess != nil {
+		y.Error(sucess)
+	}
+
+	fmt.Println(mat.Formatted(initState))
+}
+
+func TestAdaptiveComputeExceedCount(y *testing.T) {
+	odeObject := NewFehlberg45()
+	t0, t1 := 0., 10.
+	err := 1e-9
+	initState := mat.NewVecDense(9, []float64{1.2, 32., 34, 12, 532, 12, 35, 1, 0.91283})
+	data := make([]float64, 9*9)
+	for index := 0; index < 9*9; index++ {
+		data[index] = rand.Float64()
+	}
+	stateTransitionMatrix := mat.NewDense(9, 9, data)
+	inputvector := mat.NewVecDense(9, []float64{1, 0, 0, 0, 0, 0, 0, 0, 0})
+	inputs := make([]signal.VectorFunction, 1)
+	inputs[0] = signal.NewInput(func(arg1 float64) float64 { return 1.24 }, inputvector)
+
+	observationVector := mat.NewDense(1, 9, []float64{0, 0, 0, 0, 0, 0, 0, 0, 1})
+
+	sys := ssm.NewLinearStateSpaceModel(stateTransitionMatrix, observationVector, inputs)
+
+	sucess := odeObject.AdaptiveCompute(t0, t1, err, initState, sys)
+
+	if sucess == nil {
+		y.Error("Didn't produce error for extreme integration length")
+	}
+
 	fmt.Println(mat.Formatted(initState))
 }
